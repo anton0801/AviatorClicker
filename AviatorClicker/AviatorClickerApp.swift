1784af751f55b9a5fd08c5e10c84691ec8ab7245
Anton <danilovanton724@gmail.com>
2024-05-06 14:58:23 +0200

    Initial Commit

---
+++ AviatorClicker/AviatorClickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AviatorClickerApp.swift
+//  AviatorClicker
+//
+//  Created by Anton on 6/5/24.
+//
+
+import SwiftUI
+
+@main
+struct AviatorClickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AviatorClicker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AviatorClicker
+//
+//  Created by Anton on 6/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
